ea7f74c7eca035b0e9f7f32f973ce6d4511b4363
Surya Prakash <suryaprakashbt1822@gmail.com>
2022-04-06 00:45:51 +0530

    Add project files.

---
+++ ConsoleApp_LitwareLib/ConsoleApp_LitwareLib/Employee_Class.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLitwareLib
+{
+    public class Employee
+    {
+        private int EmpNo;
+        private string EmpName;
+        private double Salary;
+        private double HRA;
+        private double TA;
+        private double DA;
+        private double PF;
+        private double TDS;
+        private double NetSalary;
+        private double GrossSalary;
+
+        public int empNumber
+        {
+            get { return this.EmpNo; }
+            set { this.EmpNo = value; }
+        }
+
+        public string empName
+        {
+            get { return this.EmpName; }
+            set { this.EmpName = value; }
+        }
+
+        public double salary
+        {
+            get { return this.Salary; } 
+            set { this.Salary = value; }
+        }
+
+        public double calculate_HRA(double salary)
+        {
+            if(salary < 5000)
+            {
+                this.HRA = 0.1 * salary;
+                //return this.HRA;
+            }
+            else if(salary < 10000)
+            {
+                this.HRA = 0.15 * salary;
+            }
+            else if(salary < 15000)
+            {
+                this.HRA = 0.2 * salary;
+            }
+            else if(salary < 20000)
+            {
+                this.HRA = 0.25 * salary;   
+            }
+            else if(salary >= 20000)
+            {
+                this.HRA = 0.3 * salary;
+            }
+            return this.HRA;
+        }
+        public double calculate_TA(double salary)
+        {
+            if (salary < 5000)
+            {
+                this.TA = 0.05 * salary;
+                //return this.HRA;
+            }
+            else if (salary < 10000)
+            {
+                this.TA = 0.1 * salary;
+            }
+            else if (salary < 15000)
+            {
+                this.TA = 0.15 * salary;
+            }
+            else if (salary < 20000)
+            {
+                this.TA = 0.2 * salary;
+            }
+            else if (salary >= 20000)
+            {
+                this.TA = 0.25 * salary;
+            }
+            return this.TA;
+        }
+
+        public double calculate_DA(double salary)
+        {
+            if (salary < 5000)
+            {
+                this.DA = 0.15 * salary;
+                //return this.HRA;
+            }
+            else if (salary < 10000)
+            {
+                this.DA = 0.2 * salary;
+            }
+            else if (salary < 15000)
+            {
+                this.DA = 0.25 * salary;
+            }
+            else if (salary < 20000)
+            {
+                this.DA = 0.3 * salary;
+            }
+            else if (salary >= 20000)
+            {
+                this.DA = 0.35 * salary;
+            }
+            return this.DA;
+        }
+        public double grossSalary( double salary)
+        {
+            this.GrossSalary = salary + this.HRA + this.DA + this.TA;
+            return this.GrossSalary;
+        }
+
+        public void CalculateSalary()
+        {
+            this.PF = 0.1 * GrossSalary;
+            this.TDS = 0.18 * GrossSalary;
+            this.NetSalary = GrossSalary - this.PF - this.TDS;
+        }
+
+    }
+}
---
+++ ConsoleApp_LitwareLib/ConsoleApp_LitwareLib/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleAppLitwareLib;
+
+Console.WriteLine("------------EMPLOYEE MANAGEMENT SYSTEM FOR LITWARE ORGANISATION------------\n\n");
+
+Console.WriteLine("-------HR staff member registers newly joined Employee-------\n\n");
+
+Console.WriteLine("-------NEW EMPLOYEE REGISTRATION-------\n");
+
+Employee newEmployee=new Employee(); // new object created
+
+
+Console.WriteLine("Enter the Emp ID/Employee No. of the new Joinee");
+try
+{
+    newEmployee.empNumber = Convert.ToInt32(Console.ReadLine());
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);  
+}
+
+Console.WriteLine("Enter the Name of the new Joinee");
+try
+{
+    newEmployee.empName = Console.ReadLine();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine("Enter the Salary of the new Joinee");
+try
+{
+    newEmployee.salary = Convert.ToDouble(Console.ReadLine());
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine("\n**********DETAILS OF THE NEW EMPLOYEE**********\n");
+Console.WriteLine("Employee ID/No. : {0}",newEmployee.empNumber);
+Console.WriteLine("Employee Name : {0}", newEmployee.empName);
+double newEmployeeSalary = newEmployee.salary;
+
+//Invoking the respective methods to calculate HRA, TA, DA
+
+newEmployee.calculate_TA(newEmployeeSalary);
+newEmployee.calculate_DA(newEmployeeSalary);
+newEmployee.calculate_HRA(newEmployeeSalary);
+
+
+
+Console.WriteLine("Employee Gross Salary : {0}", newEmployee.grossSalary(newEmployeeSalary));
